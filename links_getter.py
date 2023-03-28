9e517b936bc1eca999e61b1fe085eabea0b77781
redouan45 <redouanmlk9@gmail.com>
2023-03-28 15:48:56 +0100

    made keyword search adaptive to resultant number of pages

--- links_getter.py
+++ links_getter.py
@@ -2,55 +2,57 @@ from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
 from datetime import datetime
-
+def scroll(s):
+    for i in range(0, 6):
+        s = s + 250
+        Driver.execute_script(f"window.scrollTo(0, window.scrollY + {s})")
+        time.sleep(1)
 print(datetime.now())
-options = Options
-options.headless = True
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-Driver = webdriver.Chrome(executable_path="C:/Users/HP/Desktop/chromedriver.exe",options=options,chrome_options=chrome_options)
+options1 = Options()
+options1.add_argument('--headless')
+options1.add_argument('--disable-gpu')
+options1.add_argument('--blink-settings=imagesEnabled=false')
+Driver = webdriver.Chrome(executable_path="C:/Users/HP/Desktop/chromedriver.exe",chrome_options=options1)
 total = 0
 keywords= ["208 2020",'dacia logan' ]
 for keyword in keywords:
     keyword.replace(" ", "-")
-#TODO:Rework keyword_search() such that number of results is considered
 #Can add &hasPictures=true in link
+#Can add hasPrice=true in link
+
 def keyword_search(pages):
     global total
     for keyword in keywords:
+        Driver.get(f'https://www.ouedkniss.com/automobiles/{1}?keywords={keyword}&lang=en')
+        time.sleep(3)
+        scroll(400)
+        pages_nav = Driver.find_element_by_xpath('//*[@id="search-content"]/div/div[4]/div/nav/ul')
+        if int(pages_nav.text.split('\n')[-1]) < pages :
+            pages = int(pages_nav.text.split('\n')[-1])
+            print(f'Only found {pages} Pages')
         for i in range(1, pages, 1):
             links = []
             Driver.get(f'https://www.ouedkniss.com/automobiles/{i}?keywords={keyword}&lang=en')
-            time.sleep(2)
-            s = 100
-            for i in range(0, 3):
-                s = s * 4
-                Driver.execute_script(f"window.scrollTo(0, window.scrollY + {s})")
-                time.sleep(1)
+            time.sleep(3)
+            scroll(400)
             elems = Driver.find_elements_by_xpath("//a[@href]")
             for elem in elems:
-                if not ("/store/" in elem.get_attribute("href") or "/Ooredoo" in elem.get_attribute("href")):
+                if not ("/store/" in elem.get_attribute("href") or "/Ooredoo" in elem.get_attribute("href") ):
                     links.append(elem.get_attribute("href"))
             total += len(links) - 14
             print(total)
             with open("links.txt", "a+") as file:
-                for link in links[3:len(links) - 11]:
+                for link in links[12:len(links) - 2]:
                     file.write(link + '\n')
-            time.sleep(1000)
-
+            time.sleep(1)
 
 def normal_search(pages):
     global total
     for i in range(1,pages,1):
         links = []
         Driver.get(f"https://www.ouedkniss.com/automobiles/{i}")
-
         time.sleep(2)
-        s = 100
-        for i in range(0,3):
-            s = s *4
-            Driver.execute_script(f"window.scrollTo(0, window.scrollY + {s})")
-            time.sleep(1)
+        scroll(400)
         elems = Driver.find_elements_by_xpath("//a[@href]")
         for elem in elems:
             if not ("/store/" in elem.get_attribute("href") or "/Ooredoo"in elem.get_attribute("href")):
@@ -58,7 +60,7 @@ def normal_search(pages):
         total += len(links)-14
         print(total)
         with open("links.txt","a+") as file:
-            for link in links[3:len(links)-11]:
+            for link in links[12:len(links)-2]:
                 file.write(link+'\n')
         time.sleep(1)
 
